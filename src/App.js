9459c7a4002718566ee52acb6644bc9b1983de73
sarahwxie <sarahwxie@gmail.com>
2022-10-16 04:09:55 -0700

    convert to hashrouting for github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import HomePage from "./pages/Home";
 import MusicPage from "./pages/Music";
 import NotFound from './utils/NotFound'
@@ -20,16 +21,12 @@ const App = () => {
         <React.Fragment>
             <NavBar />
         </React.Fragment>
-        <Router>
-          <div>
-              <Routes>
-                  <Route exact path={"/"} element={<HomePage/>}/>
-                  <Route exact path={"/test"} element={test}/>
-                  <Route exact path={"/music"} element={<MusicPage/>}/>
-                  <Route path="*" element={<NotFound/>}/>
-              </Routes>
-          </div>
-      </Router>
+      <HashRouter>
+         <Routes>
+           <Route exact path="/" name="Home Page" element={<HomePage/>} />
+           <Route exact path="/music" name="Music" element={<MusicPage/>} />
+         </Routes>
+     </HashRouter>
 
     </div>
   );
