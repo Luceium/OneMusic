c492015ce792ef16ef74f82d6151fe79c2a520c4
sarahwxie <sarahwxie@gmail.com>
2022-10-16 06:44:06 -0700

    spotifytest to test the spotify api -- almost there but browser started giving errors after logins

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import './App.css';
 import ReactDOM from 'react-dom/client';
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import HomePage from "./pages/Home";
 import MusicPage from "./pages/Music";
+import SpotifyTest from "./pages/spotifyTest";
+
 import NotFound from './utils/NotFound'
 import Footer from "./shared/Footer";
 import NavBar from "./shared/NavBar";
@@ -16,8 +18,10 @@ const test = (
 );
 
 const App = () => {
+
   return (
     <div className="App">
+
         <React.Fragment>
             <NavBar />
         </React.Fragment>
@@ -25,6 +29,7 @@ const App = () => {
          <Routes>
            <Route exact path="/" name="Home Page" element={<HomePage/>} />
            <Route exact path="/music" name="Music" element={<MusicPage/>} />
+           <Route exact path="/spotifyTest" name="spotifyTest" element={<SpotifyTest/>} />
          </Routes>
      </HashRouter>
 
